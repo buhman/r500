a5cb458c1f3967a871640637e9eee03b843b9568
Zack Buhman <zack@buhman.org>
2025-10-15 19:18:37 -0500

    drm: add single_color_clear_translate_vblank

--- pci_user/main.c
+++ pci_user/main.c
@@ -12,9 +12,10 @@ static inline uint32_t rreg(void * rmmio, uint32_t offset)
 {
   uint32_t value = *((volatile uint32_t *)(((uintptr_t)rmmio) + offset));
   asm volatile ("" ::: "memory");
+  return value;
 }
 
-static inline uint32_t wreg(void * rmmio, uint32_t offset, uint32_t value)
+static inline void wreg(void * rmmio, uint32_t offset, uint32_t value)
 {
   *((volatile uint32_t *)(((uintptr_t)rmmio) + offset)) = value;
   asm volatile ("" ::: "memory");
