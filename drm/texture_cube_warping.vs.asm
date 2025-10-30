9f1999fd19b16b89347c175dcb27b0fce81701fd
Zack Buhman <zack@buhman.org>
2025-10-30 15:11:46 -0500

    drm: add texture_cube_warping

---
+++ drm/texture_cube_warping.vs.asm
@@ -0,0 +1,3 @@
+temp[0].x       = VE_MUL       input[0].z___     input[0].1___     ;
+out[1].xy       = VE_ADD       input[1].xy__     input[1].0000     ;
+out[0].xyzw     = VE_ADD       temp[0].-0-0-x-0  input[0].xy01     ;
